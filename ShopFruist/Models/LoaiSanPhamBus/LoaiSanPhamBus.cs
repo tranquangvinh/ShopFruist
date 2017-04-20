6d3684019dc1a971b7a59e1fc7b8d70c3bd50df6
Tran Quang Vinh <tranquangvinh5899@gmail.com>
2017-04-20 16:52:01 +0700

    success details product

--- ShopFruist/Models/LoaiSanPhamBus/LoaiSanPhamBus.cs
+++ ShopFruist/Models/LoaiSanPhamBus/LoaiSanPhamBus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopFruistConnection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,10 @@ namespace ShopFruist.Models.LoaiSanPhamBus
 {
     public class LoaiSanPhamBus
     {
+        public static IEnumerable<LOAISANPHAM> DanhSach()
+        {
+            var db = new ShopFruistConnectionDB();
+            return db.Query<LOAISANPHAM>("select * from loaisanpham");
+        }
     }
 }
--- ShopFruist/Models/SanPhamBus/SanPhamBus.cs
+++ ShopFruist/Models/SanPhamBus/SanPhamBus.cs
@@ -15,5 +15,18 @@ namespace ShopFruist.Models
             String query = "select top " + limit + " * from sanpham order by MaSanPham " + sx;
             return db.Query<SANPHAM>(query);
         }
+
+        public static SANPHAM Details(int id)
+        {
+            var db = new ShopFruistConnectionDB();
+            return db.SingleOrDefault<SANPHAM>("select * from sanpham where MaSanPham = @0", id);
+        }
+
+        public static IEnumerable<SANPHAM> RelatedProductsDetails(int idSP, int limit,  int? LSP)
+        {
+            var db = new ShopFruistConnectionDB();
+            String query = "select top " + limit + " * from sanpham where MaSanPham not in ("  + idSP + ") and LoaiSp = " + LSP;
+            return db.Query<SANPHAM>(query);
+        }
     }
 }
